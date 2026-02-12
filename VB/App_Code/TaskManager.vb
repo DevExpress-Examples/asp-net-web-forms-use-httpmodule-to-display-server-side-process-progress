e1ccf0c25b77342c2acefef7f7d3d53a79401a95
DevExpressExampleBot <scmaintenance@devexpress.com>
2026-02-12 14:14:32 +0400

    Source auto update [skip ci]

---
+++ VB/App_Code/TaskManager.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Threading
+Imports System.Threading.Tasks
+Imports System.Collections.Generic
+
+Public Module TaskManager
+	Private _tasks As Dictionary(Of Int32, Task) = New Dictionary(Of Int32,Task)()
+	Private l_lock As New Object()
+
+	Public Function Register(ByVal task As Task) As Int32
+		_tasks.Add(task.Id, task)
+		Return task.Id
+	End Function
+
+	Public Function Register(ByVal task As Task, ByVal dispose As Boolean) As Int32
+		SyncLock l_lock
+			If dispose Then
+				task.ContinueWith(Sub(reference)
+					_tasks.Remove(reference.Id)
+					reference.Dispose()
+				End Sub)
+			End If
+			Return Register(task)
+		End SyncLock
+	End Function
+
+	Public Function FindTask(ByVal id As Int32) As Task
+		SyncLock l_lock
+			Return _tasks(id)
+		End SyncLock
+	End Function
+End Module
---
+++ VB/App_Code/Test.vb
@@ -0,0 +1,11 @@
+﻿Imports System
+Imports System.Threading
+Imports System.Threading.Tasks
+
+Public Class Test
+	Public Property Value() As Int32
+
+	Public Overrides Function ToString() As String
+		Return Value.ToString()
+	End Function
+End Class
